18c88526eda1cddd2002d323f6809b2f6fb92cc5
vikassharmaa1 <vikassharmaa1@gmail.com>
2016-11-30 21:29:02 -0500

    First commit

---
+++ JS_Practice/CurrentDate.js
@@ -0,0 +1,22 @@
+var today = new Date();  
+var dd = today.getDate();  
+  
+var mm = today.getMonth()+1;   
+var yyyy = today.getFullYear();  
+if(dd<10)   
+{  
+    dd='0'+dd;  
+}   
+  
+if(mm<10)   
+{  
+    mm='0'+mm;  
+}   
+today = mm+'-'+dd+'-'+yyyy;  
+alert(today);  
+today = mm+'/'+dd+'/'+yyyy;  
+alert(today);  
+today = dd+'-'+mm+'-'+yyyy;  
+alert(today);  
+today = dd+'/'+mm+'/'+yyyy;  
+alert(today);
---
+++ JS_Practice/index.js
@@ -0,0 +1,5 @@
+function add_numbers(a, b) {
+    return a + b;
+}
+ 
+alert("2 + 3 => " + add_numbers(2, 3));
